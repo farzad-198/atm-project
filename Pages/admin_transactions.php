4cf475e686eacd58a70751eda91f1d9a58180e19
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-05-23 17:28:27 +0200

    edite admin-transactions page

--- Pages/admin_transactions.php
+++ Pages/admin_transactions.php
@@ -4,16 +4,13 @@ session_start();
 
 require_once '../src/db.php';
 require_once '../src/functions.php';
+require_once '../src/TransactionRepository.php';
 
 require_login();
 require_admin();
 
-$stmt = $pdo->query("
-    SELECT *
-    FROM transactions
-    ORDER BY created_at DESC
-");
-$transactions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$transactionRepository = new TransactionRepository($pdo);
+$transactions = $transactionRepository->getAllTransactions();
 
 ?>
 
