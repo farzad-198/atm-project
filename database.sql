98e1e70c75271d01bf9cec5ee8d94a3d78d186cb
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-05-27 17:59:54 +0200

    update database script new

--- database.sql
+++ database.sql
@@ -1,13 +1,14 @@
 IF DB_ID(N'atm_project') IS NULL
-BEGIN
     CREATE DATABASE atm_project;
-END;
+GO
 
 USE atm_project;
+GO
 
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS users;
+GO
 
 CREATE TABLE users (
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -15,7 +16,7 @@ CREATE TABLE users (
     card_number VARCHAR(20) NOT NULL UNIQUE,
     pin_hash VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL DEFAULT 'user',
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+   created_at Timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE accounts (
@@ -23,7 +24,7 @@ CREATE TABLE accounts (
     user_id INT NOT NULL,
     account_type VARCHAR(50) NOT NULL,
     balance DECIMAL(10,2) NOT NULL DEFAULT 0,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    created_at Timestamp DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
@@ -33,5 +34,5 @@ CREATE TABLE transactions (
     amount DECIMAL(10,2) NOT NULL,
     from_account_id INT NULL,
     to_account_id INT NULL,
-    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+    created_at Timestamp DEFAULT CURRENT_TIMESTAMP
 );
